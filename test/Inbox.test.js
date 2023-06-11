0fa89c38b77262f7ca462c2d2eaa5aa40db491ec
mamismeda <amamismeda@gmail.com>
2023-06-11 23:09:58 +0400

    creating more mocha tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,3 @@
-// contract test code will go here
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
@@ -9,49 +8,29 @@ let accounts;
 let inbox;
 
 beforeEach(async () => {
-  //Get a list of all accounts
+  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-
-  // Use one of thoes accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-   .deploy({ data: bytecode, arguments: ['Hi there!'] })
-   .send({ from: accounts[0], gas: '1000000' })
-
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
-
+console.log(inbox);
 describe("Inbox", () => {
-  it("deploys a cotract", () => {
-    console.log(inbox);
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
   });
 });
-
-
-
-
-
-// class Car {
-//   park() {
-//     return "stopped";
-//   }
-
-//   drive() {
-//     return "vroom";
-//   }
-// }
-
-// let car;
-
-// beforeEach(() => {
-//    car = new Car();
-// });
-
-//"""describe""" grouping together it statements !!!
-// describe("Car", () => {
-//   it("can park", () => {
-//     assert.equal(car.park(), "stopped");
-//   });
-
-//   it("can drive", () => {
-//     assert.equal(car.drive(), "vroom");
-//   });
-// });
